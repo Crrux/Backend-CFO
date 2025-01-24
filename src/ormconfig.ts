4b0069edf9c0f08b9fe8e066f13024bc3cc22071
Mursch Arthur <arthur.mursch@gmail.com>
2025-01-24 12:19:48 +0100

    variable environnement ajouter

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,14 +1,19 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { Contact } from './contact/entities/contact.entity';
+import { ConfigService } from './config/config.service';
+
+const configService = new ConfigService({
+  folder: './config',
+});
 
 const options: DataSourceOptions = {
   type: 'mariadb',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'CrossFitObernai',
+  host: configService.get('DB_HOST'),
+  port: parseInt(configService.get('DB_PORT'), 10),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
   entities: [Contact],
 };
 
