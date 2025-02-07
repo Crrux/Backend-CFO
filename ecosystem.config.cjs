9bf8560ad92144dd055ac78404a7d1af27a7965d
Mursch Arthur <arthur.mursch@gmail.com>
2025-02-07 19:13:28 +0100

    save

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [{
+        name: 'BackEnd',
+        script: 'npm',
+        args: 'run start:prod',
+        env: {
+            NODE_ENV: 'production',
+        },
+        error_file: './logs/err.log',
+        out_file: './logs/out.log'
+    }]
+}
