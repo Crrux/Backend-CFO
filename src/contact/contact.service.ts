0cab759eaa0544cbf54f6d47de8058751865e7b1
MURSCH Arthur <149969486+Crrux@users.noreply.github.com>
2025-02-20 14:05:31 +0100

    modif contact service

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -17,28 +17,33 @@ export class ContactService {
     private mailerConfig: ConfigType<typeof mailer>,
   ) {}
 
-  async mailer(body: SendFormDto): Promise<void> {
-    const currentDate = new Date();
-    const Reference = `${currentDate.toISOString().replace(/\D/g, '')}${body.firstname[0]}${body.name[0]}${body.tel.slice(-4)}`;
-    body.reference = Reference.toUpperCase();
-    await this.mailService.sendMail({
-      from: `${this.mailerConfig.USERNAME_NAME} <${this.mailerConfig.USERNAME}>`,
-      to: `${this.mailerConfig.SUBJECT}`,
-      subject: `Formulaire CFO`,
-      html: `
-          <div>
-            <h1 style="color:red;">Formulaire Site CrossfitObernai</h1>
-            <p><span style="font-weight:bold; margin: 0; padding: 0;">Nom / Prenom:</span><p>
-            <pre>${body.name.charAt(0).toUpperCase() + body.name.slice(1).toLowerCase()} ${body.firstname.charAt(0).toUpperCase() + body.firstname.slice(1).toLowerCase()}</pre>
-            <p><span style="font-weight:bold; margin: 0; padding: 0;">Email:</span> </p>
-            <pre>${body.email}</pre>
-            <p><span style="font-weight:bold; margin: 0; padding: 0;">Téléphone:</span></p>
-            <pre>${body.tel}</pre>
-            <p><span style="font-weight:bold; margin: 0; padding: 0;">Message:</span> </p>
-            <pre>${body.message}</pre>
-            <p style="font-size:0.5rem"><span style="font-weight:bold;">Référence:</span> ${body.reference}</p>
-          </div>`,
-    });
+  async mailer(body: SendFormDto): Promise<string> {
+    try {
+      const currentDate = new Date();
+      const Reference = `${currentDate.toISOString().replace(/\D/g, '')}${body.firstname[0]}${body.name[0]}${body.tel.slice(-4)}`;
+      body.reference = Reference.toUpperCase();
+      await this.mailService.sendMail({
+        from: `${this.mailerConfig.USERNAME_NAME} <${this.mailerConfig.USERNAME}>`,
+        to: `${this.mailerConfig.SUBJECT}`,
+        subject: `Formulaire CFO`,
+        html: `
+            <div>
+              <h1 style="color:red;">Formulaire Site CrossfitObernai</h1>
+              <p><span style="font-weight:bold; margin: 0; padding: 0;">Nom / Prenom:</span><p>
+              <pre>${body.name.charAt(0).toUpperCase() + body.name.slice(1).toLowerCase()} ${body.firstname.charAt(0).toUpperCase() + body.firstname.slice(1).toLowerCase()}</pre>
+              <p><span style="font-weight:bold; margin: 0; padding: 0;">Email:</span> </p>
+              <pre>${body.email}</pre>
+              <p><span style="font-weight:bold; margin: 0; padding: 0;">Téléphone:</span></p>
+              <pre>${body.tel}</pre>
+              <p><span style="font-weight:bold; margin: 0; padding: 0;">Message:</span> </p>
+              <pre>${body.message}</pre>
+              <p style="font-size:0.5rem"><span style="font-weight:bold;">Référence:</span> ${body.reference}</p>
+            </div>`,
+      });
+      return 'Email sent';
+    } catch (error) {
+      throw new Error('Error sending email');
+    }
   }
 
   async contactBddEntry(body: SendFormDto): Promise<void> {
